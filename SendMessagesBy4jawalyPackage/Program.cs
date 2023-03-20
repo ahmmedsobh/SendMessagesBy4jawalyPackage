a59d4cdc1177158b83c056554bd2bed6fb258dee
ahmed sobh <ahmedsobhgm@gmail.com>
2023-03-20 18:57:51 +0200

    Add project files.

---
+++ SendMessagesBy4jawalyPackage/Program.cs
@@ -0,0 +1,20 @@
+﻿using ForJawalySMSHelper;
+using ForJawalySMSHelper.Models;
+
+ForJawalySMS.app_key = "app key";
+ForJawalySMS.app_secret = "app secret";
+ForJawalySMS.sender = "sender name";
+
+var root = new Root()
+{
+    text = "text",
+    numbers = new List<string>()
+    {
+        "XXXXXXXXXXX",
+    }
+};
+
+var result = await ForJawalySMS.SendSms(root);
+Console.WriteLine(result.message);
+Console.ReadKey();
+
